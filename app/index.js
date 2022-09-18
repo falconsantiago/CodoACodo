4b8edae0e8023438bd33b9b52ce00f5286c96dd6
SebastianGuelfo <62261107+SebastianGuelfo@users.noreply.github.com>
2022-09-18 01:22:44 -0300

    first commit

---
+++ app/index.js
@@ -0,0 +1,11 @@
+// nav functions
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+// nav functions
