64459f9e2b144cfa732a80fa6a44ddd4cd7c1c46
SebastianGuelfo <62261107+SebastianGuelfo@users.noreply.github.com>
2022-10-13 02:02:25 -0300

    no puedo resolver por que innerhtml no funciona...

--- app/index.js
+++ app/index.js
@@ -1,29 +1,94 @@
 // nav functions
-
 function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
+	document.getElementById("mySidenav").style.width = "250px";
 }
-
 function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
+	document.getElementById("mySidenav").style.width = "0";
 }
 
 // nav functions
 
-var vid = document.getElementById("myVideo");
-vid.playbackRate = 0.75;
+//activar esto cuando termine con la parte de APIS
 
-let flag= false
+// let vid = document.getElementById("myVideo");
+// vid.playbackRate = 0.75;
+// let flag = false;
 
-function openForm(){
-
-  
-let form=document.getElementById("form")
-if (flag===false){
-  form.style.height="500px"
-  flag=true
-} else if(flag===true){
-  form.style.height="0"
-  flag=false
+function openForm() {
+	let form = document.getElementById("form");
+	if (flag === false) {
+		form.style.height = "500px";
+		flag = true;
+	} else if (flag === true) {
+		form.style.height = "0";
+		flag = false;
+	}
 }
+//api functions
+
+function apiCall(difficulty) {
+	const apiKey = "yx3jbPTs9/VpvjlI+qvb5w==tkZsqvGlhJaACr6g";
+
+	if (difficulty === "beginner") {
+		$.ajax({
+			method: "GET",
+			url:
+				"https://api.api-ninjas.com/v1/exercises?type=cardio" +
+				"&difficulty=" +
+				difficulty,
+			headers: { "X-Api-Key": apiKey },
+			contentType: "application/json",
+			success: function (result) {
+				console.log(result);
+				return (beginnerRes = result[2]);
+			},
+			error: function ajaxError(jqXHR) {
+				console.error("Error: ", jqXHR.responseText);
+			},
+		});
+	} else if (difficulty === "intermediate") {
+		$.ajax({
+			method: "GET",
+			url:
+				"https://api.api-ninjas.com/v1/exercises?type=cardio" +
+				"&difficulty=" +
+				difficulty,
+			headers: { "X-Api-Key": apiKey },
+			contentType: "application/json",
+			success: function (result) {
+				console.log(result);
+				return (intermediateRes = result[2]);
+			},
+			error: function ajaxError(jqXHR) {
+				console.error("Error: ", jqXHR.responseText);
+			},
+		});
+	}
 }
+
+// let beginnerRes = apiCall("beginner")[2];
+apiCall("beginner");
+apiCall("intermediate");
+// apiCall("expert");
+
+let markup = `
+  				<div class="routine-card" id="routineCard">
+  					<h1>Some excercises for this race!</h1>
+  					<h3>${beginnerRes.name}</h3>
+  					<span>${beginnerRes.muscle}</span>
+ 				<span>${beginnerRes.equipment}</span>
+ 				<p>${beginnerRes.instructions}</p>
+ 			</div>`;
+
+function routine(e) {
+	if (e === "SHORT") {
+		console.log(beginnerRes);
+	} else if (e === "MID") {
+		console.log(intermediateRes);
+	} else {
+		console.log("expert");
+	}
+}
+//el expert no tiene data
+
+document.body.innerHTML = "";
--- app/races.js
+++ app/races.js
@@ -1,66 +1,71 @@
-var races= [
-    {
-    "id": 1,
-    "race": "UTHC 125",
-    "presenter": "The North Face",
-    "start": "Friday 13:00",
-    "Distance": "122 km",
-    "Elevation": "+4220m / -4580m",
-    "Cut-off time": "29 hs",
-    "Stations": "10",
-    "Course": "Linear",
-    "Qualifying-races": "Western States 100, ITRA 5"
-    },
-    {
-        "id": 2,
-        "race": "UTHC 80",
-        "presenter": "",
-        "start": "Saturday 7:00 AM",
-        "Distance": "78 km",
-        "Elevation": "+2146m / -1976m",
-        "Cut-off time": "15hs30",
-        "Stations": "7",
-        "Course": "Linear",
-        "Qualifying-races": "ITRA 3"
-        },
-        {
-            "id": 3,
-            "race": "UTHC 65",
-            "presenter": "Guru",
-            "start": "Saturday 8:30 AM",
-            "Distance": "63 km",
-            "Elevation": "+1870m / -1550m",
-            "Cut-off time": "12hs15",
-            "Stations": "5",
-            "Course": "Linear",
-            "Qualifying-races": "ITRA 3"
-            },
-            {
-                "id": 4,
-                "race": "UTHC 42",
-                "presenter": "",
-                "start": "Saturday 7:00 AM",
-                "Distance": "42,7 km",
-                "Elevation": "+1300m / -1290m",
-                "Cut-off time": "8hs30",
-                "Stations": "4",
-                "Course": "Linear",
-                "Qualifying-races": "ITRA 2"
-                },
-                {
-                    "id": 5,
-                    "race": "Trail 28 km",
-                    "presenter": "La Boutique du Lac",
-                    "start": "Saturday 9:00 hs",
-                    "Distance": "28 km",
-                    "Elevation": "1000m D+",
-                    "Cut-off time": "5hs30",
-                    "Stations": "2",
-                    "Course": "Loop",
-                    "Qualifying-races": "ITRA 1"
-                    },
+var races = [
+	{
+		id: 1,
+		race: "UTHC 125",
+		presenter: "The North Face",
+		start: "Friday 13:00",
+		Distance: "122 km",
+		Elevation: "+4220m / -4580m",
+		"Cut-off time": "29 hs",
+		Stations: "10",
+		Course: "Linear",
+		"Qualifying-races": "Western States 100, ITRA 5",
+	},
+	{
+		id: 2,
+		race: "UTHC 80",
+		presenter: "",
+		start: "Saturday 7:00 AM",
+		Distance: "78 km",
+		Elevation: "+2146m / -1976m",
+		"Cut-off time": "15hs30",
+		Stations: "7",
+		Course: "Linear",
+		"Qualifying-races": "ITRA 3",
+	},
+	{
+		id: 3,
+		race: "UTHC 65",
+		presenter: "Guru",
+		start: "Saturday 8:30 AM",
+		Distance: "63 km",
+		Elevation: "+1870m / -1550m",
+		"Cut-off time": "12hs15",
+		Stations: "5",
+		Course: "Linear",
+		"Qualifying-races": "ITRA 3",
+	},
+	{
+		id: 4,
+		race: "UTHC 42",
+		presenter: "",
+		start: "Saturday 7:00 AM",
+		Distance: "42,7 km",
+		Elevation: "+1300m / -1290m",
+		"Cut-off time": "8hs30",
+		Stations: "4",
+		Course: "Linear",
+		"Qualifying-races": "ITRA 2",
+	},
+	{
+		id: 5,
+		race: "Trail 28 km",
+		presenter: "La Boutique du Lac",
+		start: "Saturday 9:00 hs",
+		Distance: "28 km",
+		Elevation: "1000m D+",
+		"Cut-off time": "5hs30",
+		Stations: "2",
+		Course: "Loop",
+		"Qualifying-races": "ITRA 1",
+	},
+];
 
-
-
-
-]
+races.forEach((e) => {
+	let distNum = parseInt(e.Distance);
+	if (distNum > 63) {
+		return e.id;
+	} else if (distNum < 63 && distNum > 43) {
+		return e.id;
+	}
+});
